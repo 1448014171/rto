18575fb9a20a361b32dfc3693c055c7d4cb488fe
1448014171 <127064214+1448014171@users.noreply.github.com>
2023-06-01 15:08:01 +0800

    Update enquireforms.js

--- enquireforms.js
+++ enquireforms.js
@@ -1,8 +1,27 @@
-function go(){
-    var run=document.getElementById("myinput").value
-    if(run===""||run == null){
-        alert("文本框为空");
-    
-    }else alert("已成功提交");
-
-}
+function go() {
+    const name = document.getElementById("inputone").value.trim();
+    const schoolID = document.getElementById("inputtwo").value.trim();
+    const phonenumber = document.getElementById("inputthree").value.trim();
+    const message = document.getElementById("inputfour").value.trim();
+    if (!name || !schoolID || !phonenumber || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    if (isNaN(phonenumber)) {
+      alert("Phone number must be numeric!");
+      return;
+    }
+    if (isNaN(schoolID)) {
+        alert("school ID must be numeric!");
+        return;
+    }
+    if (!/^[a-zA-Z]+$/.test(name) ) {
+      alert(" name must contain only letters!");
+      return;
+    }
+    if (message.split(" ").length >100) {
+      alert("Message must be a maximum of 100 words!");
+      return;
+    }
+    alert("succeed!");
+  }
